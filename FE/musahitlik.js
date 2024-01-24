2c2db5b92d35a3d6a53829c4bd1c2ed2050b9ed1
F. Serdar Atalay <fserdaratalay@gmail.com>
2024-01-25 01:28:47 +0300

    Add style from Figma and check null fields
    Figma'daki mobile görünüm için de düzenlemeli ve doğum tarihini kontrol etmeli

--- FE/musahitlik.js
+++ FE/musahitlik.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
+    $("#mahalle_select").parent().hide();
+
     $.get("http://127.0.0.1:8000/ils/", (data) => {
         for(var k in data) {
-            // console.log(k, data[k]);
 
             var x = document.getElementById("il_select");
             var option = document.createElement("option");
@@ -16,9 +17,16 @@ $(document).ready(function() {
         var il_val= $(this).val();
         $("#ilce_select").empty();
 
+        var x = document.getElementById("ilce_select");
+        var option = document.createElement("option");
+        option.value = "";
+        option.text = "İlçe Seçiniz";
+        option.disabled = true;
+        option.selected = true;
+        x.add(option);
+
         $.get("http://127.0.0.1:8000/ilces/?il_id=" + il_val, (data) => {
             for(var k in data) {
-                // console.log(k, data[k]);
 
                 var x = document.getElementById("ilce_select");
                 var option = document.createElement("option");
@@ -36,7 +44,6 @@ $(document).ready(function() {
         
         $.get("http://127.0.0.1:8000/muhtarliks/?ilce_id=" + ilce_val, (data) => {
             for(var k in data) {
-                // console.log(k, data[k]);
 
                 var x = document.getElementById("mahalle_select");
                 var option = document.createElement("option");
@@ -46,9 +53,12 @@ $(document).ready(function() {
                 x.add(option);
             }
         });
+
+        $("#mahalle_select").parent().show();
+
     });
 
-    $("#musahit_button").on("click", function() {
+    $("#musahit_button").on("click", function(e) {
 
         var tc_no = $("#tc_no").val();
         var first_name = $("#first_name").val();
@@ -60,6 +70,9 @@ $(document).ready(function() {
         var il_id = $("#il_select").val();
         var ilce_id = $("#ilce_select").val();
         var muhtar_id = $("#mahalle_select").val();
+        var profession = $("#profession").val();
+        var education = $("#education").val();
+        var extra = $("#extra").val();
 
         var data = { 
             "tc_no": tc_no,
@@ -73,15 +86,34 @@ $(document).ready(function() {
 
             "il_id": parseInt(il_id),
             "ilce_id": parseInt(ilce_id),
-            "muhtarlik_id": parseInt(muhtar_id)
+            "muhtarlik_id": parseInt(muhtar_id),
+
+            "education": education,
+            "profession": profession,
+            "extra": extra
         };
+
+        var isValid = true;
+        $(this).find(':input[required]').each(function() {
+            if ($(this).val() === '') {
+                isValid = false;
+            }
+        });
+
+        if (!$("#kvkk").is(':checked')) {
+            isValid = false;
+        }
+
+        if (!isValid) {
+            e.preventDefault();
+            alert('Lütfen tüm gerekli alanları doldurun ve KVKK metnini kabul edin.');
+        }
 
         $.ajax({
             type: 'POST',
             url: 'http://127.0.0.1:8000/musahit/',
             data: JSON.stringify(data),
             success: function(result) {
-                console.log(result);
                 $('#successModal').modal('show');
             },
             contentType: "application/json",
